108cf05a1dd55891aae7b59fe508afb8aa9fcda9
paul <sl.zhou@upltv.com>
2018-11-09 20:32:13 +0800

    make html

--- build/html/_static/js/custom.js
+++ build/html/_static/js/custom.js
@@ -4,3 +4,5 @@ $(".ethical-sidebar").hide()
 
 $('.rst-current-version').addClass('shift-up').removeAttr('data-toggle').hide()
 $('.rst-other-versions').css("display", "block")
+
+$('.wy-side-nav-search input[type=text]').removeAttr('placeholder').attr('placeholder', '搜索')
